5faadb2590a73c9fb310569d16b457767e207a11
SpiffTrip <95441222+SpiffTrip@users.noreply.github.com>
2022-06-06 15:54:39 -0700

    added functionality

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,8 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+      host: 'local',
+      password: '',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
